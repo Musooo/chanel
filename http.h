f8d8e26ab6dc12d0b4a577c659dc48ff6343fb69
Musooo <nicoloblack2@gmail.com>
2025-04-01 14:54:51 -0400

    some initial http stuff

--- http.h
+++ http.h
@@ -1,19 +1,61 @@
 #ifndef HTTP_H
 #define HTTP_H
 
-struct request {
+enum status
+{
+        CONTINUE = 100,
+        SWITCHING_PROTOCOLS,
+        EARLY_HINTS,
+        OK = 200,
+        CREATED,
+        ACCEPTED,
+        NON_AUTHORITATIVE_INFORMATION,
+        NO_CONTENT,
+        RESET_CONTENT,
+        PARTIAL_CONTENT,
+        MULTI_STATUS,
+        ALREADY_REPORTED,
+        IM_USED = 226,
+        MULTIPLE_CHOICE = 300,
+        BAD_REQUEST = 400,
+        UNAUTHORIZED,
+        PAYMENT_REQUIRED,
+        FORBIDDEN,
+        NOT_FOUND,
+        INTERNAL_SERVER_ERROR = 500,
+};
+
+enum head_name
+{
+        ACCEPT,
+        CONTENT_ENCODING,
+        CONTENT_LENGTH,
+        CONTENT_TYPE,
+};
+
+struct request
+{
         char *method;
         char *url;
         char *version;
-        header h;
-        body b;
+        struct header h;
+        /* the header and the body are separeted by a empty line*/
+        struct body b;
 };
 
-struct header{
-        char *heads;
+struct header
+{
+        struct head *headers;
 };
 
-struct body{
+struct head
+{
+        enum head_name head_key;
+        char *value;
+};
+
+struct body
+{
         char *body;
 };
 
